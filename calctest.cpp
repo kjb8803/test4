9e87028f75ceca86b9936157fb2a2f7284d7ed89
kjb8803 <47177284+kjb8803@users.noreply.github.com>
2019-03-04 17:30:03 +0900

    Create calctest.cpp

---
+++ calctest.cpp
@@ -0,0 +1,32 @@
+#include <cppunit/extensions/HelperMacros.h>
+#include "StdAfx.h"
+#include "calc.cpp"
+
+class calcTest : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(calcTest);
+    CPPUNIT_TEST( testAdd );
+    CPPUNIT_TEST_SUITE_END();
+public:
+    CalculatorTest(void);
+    ~CalculatorTest(void);
+
+    void setUp();
+    void tearDown();
+    void testAdd();
+
+private:
+    calc m_calc;
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( CalculatorTest );
+
+CalculatorTest::CalculatorTest(void){}
+CalculatorTest::~CalculatorTest(void){}
+void CalculatorTest::setUp(){}
+void CalculatorTest::tearDown(){}
+
+void CalculatorTest::testAdd()
+{
+    CPPUNIT_ASSERT(m_calc.add(4, 5) == 9);
+}
