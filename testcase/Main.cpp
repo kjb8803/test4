88a65d628bcd80ee7f5ed5bd904745c5295d7791
kjb8803 <47177284+kjb8803@users.noreply.github.com>
2019-03-04 17:50:30 +0900

    Create Main.cpp

---
+++ testcase/Main.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    CPPUNIT_NS::TextUi::TestRunner runner;
+    CPPUNIT_NS::Test* suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
+
+    runner.addTest( suite );
+
+    runner.setOutputter( new CPPUNIT_NS::CompilerOutputter( &runner.result(), CPPUNIT_NS::stdCOut() ));
+    bool wasSucessful = runner.run();
+
+    return wasSucessful ? 0 : 1;
+}
