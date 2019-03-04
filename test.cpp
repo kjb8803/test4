fbfa56f55e2b645d3c414c022a5c93c5ab17cd59
kjb8803 <47177284+kjb8803@users.noreply.github.com>
2019-03-04 16:13:29 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
+class calc
+{
+  public:
+  calc();
+  ~calc();
+  int add(int x, int y);
+};
+
+int add(int x, int y)
+{
+  return (x+y);
+}
+
 int main()
 {
   char *p = new char[8];
-  int i = 3;
+  int i = 3, j;
   
   if(i == 3)
-    printf("print ok\n");
-  else
-    printf("print not ok");
+  {
+    printf("print ok 7\n");
+    j = add(2,2+i);
+    printf("result = %d",j);
+  }else
+  {
+    printf("print not ok 7");
+    j = add(1,2);
+    printf("result = %d",j);
+  }
   
   pause();
 
