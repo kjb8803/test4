7be651aca67229c88942f4022003352cf16f460f
kjb8803 <47177284+kjb8803@users.noreply.github.com>
2019-03-05 11:11:26 +0900

    Update and rename calc.cpp to calc.h

---
+++ calc.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+
+class calc
+{
+  public:
+  calc();
+  ~calc();
+  int add(int x, int y);
+};
+
+calc::calc(){}
+calc::~calc(){}
+
+int calc::add(int x, int y)
+{
+  return (x+y);
+}
+
+
