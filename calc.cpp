434dc6ef8cb19c6fe1fa47176bf127e77c4dc882
kjb8803 <47177284+kjb8803@users.noreply.github.com>
2019-03-05 09:27:05 +0900

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -23,19 +23,19 @@ int calc::add(int x, int y)
 int main()
 {
   char *p = new char[8];
-  int i = 3;
+  int i = 3,k;
   calc j;
   
   if(i == 3)
   {
     printf("print ok 7\n");
-    j.add(2,2+i);
-    cout << "result = " << j << endl;
+    k = j.add(2,2+i);
+    printf("result = %d\n",k);
   }else
   {
     printf("print not ok 7");
-    j.add(1,2);
-    cout << "result = " << j << endl;
+    k = j.add(1,2);
+    printf("result = %d\n",k);
   }
   
   pause();
