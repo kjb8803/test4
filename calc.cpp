0e071f6f5faf8a858c0e162dfa8c8437f863e40d
kjb8803 <47177284+kjb8803@users.noreply.github.com>
2019-04-10 15:09:17 +0900

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -2,23 +2,16 @@
 
 int main()
 {
-  char *p = new char[8];
-  int i = 3,k;
+  //char *p = new char[8];
+  int i,k,m = 1;
   calc j;
-  
-  if(i == 3)
-  {
-    printf("print ok 7\n");
-    k = j.add(2,2+i);
-    printf("result = %d\n",k);
-  }else
-  {
-    printf("print not ok 7");
-    k = j.add(1,2);
-    printf("result = %d\n",k);
-  }
-  
-  //pause();
 
+  while(m == 0)
+  {
+    scanf("%d %d",i,k);
+    m = j.add(i,k);
+    printf("result = %d\n",m);
+  }
+  //pause();
   return 0;
 }
